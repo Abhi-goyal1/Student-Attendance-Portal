40095f287f3348521472f38da33a0772587e2d6b
abhi <abhigoyal72621289@gmail.com>
2022-11-26 18:21:31 +0530

    one

--- README.md
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-# Student-Attendance-Portal
-=======
-Student-Attendance-Portal
->>>>>>> 5e9773a7e2fa398a049b9b5700037db42cdb415e
--- form-handler.php
+++ form-handler.php
@@ -5,7 +5,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'Sap@gmail.com';
+$email_from = 'abhigoyal7262189@gmail.com';
 $email_subject = 'New Form Submission';
 $email_body = "User Name: $name.\n".
                "User Email : $visitor_email.\n".
